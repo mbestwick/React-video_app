1b12806ed7d4ec4b886fe2b32bc994644859d542
Michela Bestwick <michelabestwick@Michelas-MacBook-Pro.local>
2017-07-16 08:33:42 -0700

    slight changes to search bar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,21 +6,20 @@ class SearchBar extends Component {
     this.state = { term: '' };
   }
 
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange({term});
+  }
+
   render() {
     return (
       <div className="search-bar">
         <input
-          value = {this.state.term}
-          onChange={event => this.onInputChange(event.target.value)} />
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}/>
       </div>
     );
   }
-
-  onInputChange(term){
-    this.setState({term});
-    this.props.onSearchTermChange(term);
-  }
-
 }
 
 export default SearchBar;
